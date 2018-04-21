7dab58ff052313e4d2dafba6f109c9b3f984f4e3
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-04-21 13:11:51 +0200

    Adds a user model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,10 @@
+import {Schema, model} from "mongoose";
+
+const UserSchema = new Schema({
+    name: {type: String},
+    surname: {type: String},
+    email: {type: String, unique: true},
+    password: {type: String},
+});
+
+export const UserModel = model("User", UserSchema);
