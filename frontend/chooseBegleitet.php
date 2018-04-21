de5288f47dbb90996ba78787cad750871d044f75
Tim0o <5mantei@informatik.uni-hamburg.de>
2018-04-21 19:41:10 +0200

    Begleitet hinzugefügt

---
+++ frontend/chooseBegleitet.php
@@ -0,0 +1,13 @@
+<?php
+require_once("vendor/autoload.php");
+
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/lib/templates');
+$twig = new \Twig_Environment($loader);
+
+echo $twig->render('chooseBegleitet.twig',
+    [
+        'pageTitle' => 'HHLAcleaner',
+        'pageHeading' => 'Do you want to supervise the cleaning?',
+        'headingSize' => '2',
+    ]
+);
