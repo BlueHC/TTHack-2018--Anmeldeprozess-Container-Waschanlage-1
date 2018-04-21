7f7a928b6849156ee7812aad40cb5ceaa394edbb
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-04-21 11:24:32 +0200

    Adds mongo connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
 import {App} from "./app";
 import {logger} from "./utils/logger";
 import {PORT} from "./config/config";
+import {MongoService} from "./service/mongoService";
+import {mongoService} from "./types/services/mongoService";
 
 const startServer = () => {
     global.Promise = require("bluebird");
     App.listen().then(() => {
         logger.info("App is running!", {PORT});
+        return MongoService.init();
+    }).then((mongoService: mongoService) => {
+        logger.debug("Mongo Connection Established");
     }).catch((err: Error) => {
         logger.error("There was an error starting up the server", {error: err});
         process.exit(-1);
